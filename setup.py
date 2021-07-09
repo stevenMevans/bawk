2b015b0a74a1a8e846cd4d811d249fc79aa56a2d
Steven Evans <steve.michael.evans@gmail.com>
2021-07-09 13:28:30 -0400

    Add folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A keyword detection system for custom wake words.',
+    author='SFourthbrain Spotifify Triad',
+    license='',
+)
