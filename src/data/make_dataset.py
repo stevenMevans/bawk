2df0edb03ac2769efc836bc816e2c6cf349cbd51
Maury Turay <Maury@Maurys-MacBook-Pro.local>
2021-07-28 21:20:26 -0700

    segment dataset into workable chunks

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,30 +1,65 @@
-# -*- coding: utf-8 -*-
-import click
-import logging
 from pathlib import Path
-from dotenv import find_dotenv, load_dotenv
+from pathlib import PosixPath
+from random import sample
+import pandas as pd
+import shutil
+
+#use same random seed to have identical results
+random_seed = 23
 
 
-@click.command()
-@click.argument('input_filepath', type=click.Path(exists=True))
-@click.argument('output_filepath', type=click.Path())
-def main(input_filepath, output_filepath):
-    """ Runs data processing scripts to turn raw data from (../raw) into
-        cleaned data ready to be analyzed (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
+#Ratio of files to use from dataset. i.e.  sample_ratio = 1000 takes 1/1000th of files as dataset
+sample_ratio = 100
+
+##Audio File Directories
+#Base directory with audio files
+base_dir = Path('cv-corpus-6.1-singleword')
+
+#Drill into english
+english_dir = base_dir / 'en'
+english_dir.resolve()
+
+#audio clips container
+clips_dir = english_dir / 'clips'
+clips_dir.resolve()
+
+data_types = ['test','train','validated']
+
+#return list of files to use for dataset
+def ingest_tsv(tsv_file):
+    read_tsv =  pd.read_csv(str(tsv_file), sep='\t',usecols=['path','sentence','up_votes','down_votes'])
+
+    #sample at correct ratio
+    return (tsv_file.stem,read_tsv.sample( n = int(read_tsv.shape[0]/sample_ratio), random_state = random_seed))
+
+def move_files(tsv_set):
+    
+    path = Path(tsv_set[0])
+
+    #clean up old files
+    if path.exists() and path.is_dir():
+        [x.unlink() for x in path.iterdir()]
+        path.rmdir()
+    
+    #recreate directory
+    path.mkdir()
+    
+    #copy files to data directories
+    for x in tsv_set[1]['path']:
+        file = clips_dir / x
+        shutil.copy(file, path)
+
+    tsv_set[1].to_csv(index=False, path_or_buf = tsv_set[0] + '/contents.csv',columns = ['path','sentence'])
+
+def main():
+    tsv_paths= [ PosixPath(english_dir,x + '.tsv') for x in data_types]
+    
 
 
-if __name__ == '__main__':
-    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
-    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    tsv_files = [ingest_tsv(x) for x in tsv_paths]
+    [move_files(x) for x in tsv_files]
+    #print (tsv_files)
 
-    # not used in this stub but often useful for finding various files
-    project_dir = Path(__file__).resolve().parents[2]
-
-    # find .env automagically by walking up directories until it's found, then
-    # load up the .env entries as environment variables
-    load_dotenv(find_dotenv())
-
+if __name__ == "__main__":
     main()
+
