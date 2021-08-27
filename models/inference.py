e0fc70e313ab5d10bcdcdb3a28b91928fb21014d
Dami Osoba <eosoba@gmail.com>
2021-08-26 18:27:04 -0700

    add self-contained inference file

--- models/inference.py
+++ models/inference.py
@@ -5,7 +5,48 @@ import torch.nn.functional as Fi
 import torchaudio
 import torchaudio.transforms as T
 
-from constants import *
+hidden_size = 100
+sample_rate = 16000
+
+EOS_token =27
+SOS_token =28
+
+dictOfindex = {0: ' ',
+ 1: 'a',
+ 2: 'b',
+ 3: 'c',
+ 4: 'd',
+ 5: 'e',
+ 6: 'f',
+ 7: 'g',
+ 8: 'h',
+ 9: 'i',
+ 10: 'j',
+ 11: 'k',
+ 12: 'l',
+ 13: 'm',
+ 14: 'n',
+ 15: 'o',
+ 16: 'p',
+ 17: 'q',
+ 18: 'r',
+ 19: 's',
+ 20: 't',
+ 21: 'u',
+ 22: 'v',
+ 23: 'w',
+ 24: 'x',
+ 25: 'y',
+ 26: 'z',
+ 27: 'EOS',
+ 28: 'SOS',
+ 29: 'PAD'}
+
+MAX_LENGTH = 401
+max_duration = 4
+mels_dims = 80
+window_sz = 25
+skip = 10
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
