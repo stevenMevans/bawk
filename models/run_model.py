99a96f328664fbf07a23c8d615a67127767c2cba
Dami Osoba <eosoba@gmail.com>
2021-09-10 21:26:24 -0700

    new adds

--- models/run_model.py
+++ models/run_model.py
@@ -1,3 +1,5 @@
+import pandas as pd
+
 from seqtoseq import *
 from create_dataset import preprocess
 from train import trainIters
@@ -7,11 +9,14 @@ import pickle
 import json
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-epochs= 100000
-print_evy = 1000
-model_path = "model_simple_small"
+epochs= 5
+print_evy = 1
+model_path = "model_large"
+# reload = f"{model_path}_v5"
+reload = None
+# print(reload)
 
-transformed_dataset = preprocess(train_manifest_path='/Users/dami.osoba/work/cv-corpus-6.1-2020-12-11/en/commonvoice_fourth_small_manifest.json')
+transformed_dataset = preprocess(train_manifest_path='/home/ubuntu/commonvoice_validated_manifest.json', cloud=True)
 
 encoder1 = EncoderRNN(mels_dims*MAX_LENGTH, 256).to(device)
 
@@ -20,28 +25,25 @@ decoder1 = AttnDecoderRNN(256, 29, dropout_p=0.1).to(device)
 
 
 
-# encoder1 = Encoder(80, 40, 2, dropout=0.05, bidirectional=True).to(device)
-# decoder1 = Decoder(vocab_size=29, embedding_dim=15, hidden_size=20, num_layers=2).to(device)
+# encoder1 = Encoder(80, 512, 2, dropout=0.1, bidirectional=True).to(device)
+# decoder1 = Decoder(vocab_size=29, embedding_dim=100, hidden_size=1024, num_layers=4).to(device)
 
 
 
-trainIters(transformed_dataset,encoder1, decoder1, n_iters=epochs,  model_save_path=model_path, print_every=print_evy,
-           learning_rate=0.001, reload_path=None)
-
+trainIters(transformed_dataset,encoder1, decoder1, n_iters=epochs,model_save_path=model_path, print_every=print_evy,
+                        learning_rate=0.001,reload_path=reload)
 evaluateRandomly(transformed_dataset,encoder1, decoder1,10)
 
-# reload(transformed_dataset,encoder1,decoder1, 500, print_every=100, learning_rate=0.0001)
 
 
 
-# file
 # with open('output/encoder_vars_simple.pkl', 'wb') as convert_file:
 #     pickle.dump(encoder1,convert_file)
-#
+
 # with open('output/decoder_vars_simple.pkl', 'wb') as convert_file:
 #     pickle.dump(decoder1,convert_file)
-#
-#
+
+
 enc_path = f"output/enc_{model_path}"
 torch.save(encoder1.state_dict(), enc_path)
 dec_path = f"output/dec_{model_path}"
